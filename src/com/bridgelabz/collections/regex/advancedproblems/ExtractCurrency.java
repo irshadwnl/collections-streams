9cddac7cfa4da07ee0eef67f4492b33615bd3006
irshadwnl <irshadwnl07@gmail.com>
2025-03-26 12:08:00 +0530

    [Irshad] added solution of regex advanced problems

--- src/com/bridgelabz/collections/regex/advancedproblems/ExtractCurrency.java
+++ src/com/bridgelabz/collections/regex/advancedproblems/ExtractCurrency.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 public class ExtractCurrency {
     public static void main(String[] args) {
+        // example
         String text="The price is $45.99, and the discount is 10.50.";
         Pattern  p=Pattern.compile("\\$?\\d+(\\.\\d{2})?");
         Matcher m= p.matcher(text);
